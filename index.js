e0cb1cdcf8bf0c0344c6152615ce96b5641dd587
Vanessa Wagner <vmwagner@gmail.com>
2022-05-19 08:32:05 -0500

    commit tree 3f07ce17d89c919a53e16382865df581c691a4ea

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+//function superbowlWin(record) {
+//    return record.result === "W"
+//}
+
+//let win = record.find(superbowlWin)
+//    console.log(win)
+
+//function superbowlWin(record) {
+//var win = record.find(win = win.result === "W").year
+//    return win
+//}
+
+function superbowlWin(record) {
+    let win = record.find(record => record.result === "W");
+    //return win.year
+    if (win === undefined){
+        return undefined
+    }
+    if (win.result === "W"){
+        return win.year
+    }
+
+}
